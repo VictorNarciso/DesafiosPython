446c18ebaba7c56d4b95e698b2f7c072090a1baf
VictorNarciso <victor.tecnologiageral@hotmail.com>
2021-02-22 22:17:37 -0300

    exercicio de op logicas em python

---
+++ exercise.py
@@ -0,0 +1,11 @@
+##Desafios Operadores Aritméticos
+
+## Qual o percentual das despesas sobre o seu salário?
+
+salario = 4000.00
+dividas = 2800.00
+
+percentual_despesas = dividas / salario * 100
+
+print('O percentual mensal de despesas é', percentual_despesas)
+
